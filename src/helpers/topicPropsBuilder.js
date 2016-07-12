9bebc7728a8a4c37124f78ff2539b7d904fb9bdb
Hadyk, Alan <alan.hadyk@sap.com>
2016-07-12 17:18:22 +0200

    WOO-6700 : Add author and date

--- src/helpers/topicPropsBuilder.js
+++ src/helpers/topicPropsBuilder.js
@@ -26,8 +26,14 @@ function topicPropsBuilder(regEntry, sourceEntry, config) {
   //internal name all lower case without spaces
   const shortNameInternal = regEntry.nameInternal ? misc.trimAdvanced(nameInternal) : shortName;
 
+  //author of the docu topic.
+  const author = regEntry.author;
+
   //description of the docu topic.
   const description = regEntry.description;
+
+  //creation date of the docu topic.
+  const date = regEntry.date;
 
   //type of the content
   const type = misc.trimAdvanced(regEntry.type);
@@ -255,7 +261,9 @@ function topicPropsBuilder(regEntry, sourceEntry, config) {
     nameInternal,
     shortName,
     shortNameInternal,
+    author,
     description,
+    date,
     type,
     envLink,
     docuUrl,
