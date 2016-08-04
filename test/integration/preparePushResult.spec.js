923e85ca3344b8374e23b0d36212c2453587f735
Przezorski, Lukasz <lukasz.przezorski@sap.com>
2016-08-04 14:27:40 +0200

    Working version. Tests update

--- test/integration/preparePushResult.spec.js
+++ test/integration/preparePushResult.spec.js
@@ -44,7 +44,7 @@ describe('Check if backup works for full generation', () => {
 
 
   /**
-   * Check if notClonedRepositories file was created and contains content (cloneDocuSources -> _createMatrixWithNotClonedRepositories)
+   * Check if notClonedRepositories file was created and contains content (cloneDocuSources -> _createMatrixWithRepositories)
    */
   it('It should create notClonedRepositories.json file', (done) => {
 
@@ -120,6 +120,93 @@ describe('Check if backup works for full generation', () => {
   });
 
 });
+
+
+describe('Check if backup works for independent document generation', () => {
+
+  let registry;
+
+  before((done) => {
+    prepareRegistry([{'type':'services', 'name':'Failing Ipsum'}, {'type':'services', 'name':'Samuel L Ipsum'}], config, () => {
+
+      registry = testHelper.getRegistry(config.registry.shortRegistryPath);
+
+      const opt = {
+        'src': `${config.skeletonOutDestination}/**`,
+        'dest': config.generationResult.clonedResultFolderPath,
+        'branch': config.generationResult.branch,
+        'message': 'Push operation for the whole Dev Portal',
+        'independent': true,
+        'tempLocation': config.tempLocation
+      };
+
+      cloneDocuSources(registry, config, true, () => {
+        preparePlaceholders(registry, config, () => {
+          createMetaInfo(registry, null, config, () => {
+            cloner.cloneRepo(config.generationResult.srcLocation, 'preparePushResultTest', './out', () => {
+              preparePushResult(opt, done);
+            });
+          });
+        });
+      });
+    });
+  });
+
+  /**
+   * Check if indepenedentDocuRepositories file was created and contains content (cloneDocuSources -> _createMatrixWithRepositories)
+   */
+
+  it('It should create indepenedentDocuRepositories.json file', (done) => {
+    _checkFileOrDir(true, `${config.tempLocation}/indepenedentDocuRepositories.json`, true);
+    done();
+  });
+
+
+  /**
+   * Check if deletePreviouslyClonedResultsRepo operation has been performed (preparePushResult -> deletePreviouslyClonedResultsRepo)
+   */
+  it('DeleteIpsum service folder should not be deleted', (done) => {
+    _checkFileOrDir(false, `${config.tempLocation}/${config.generationResult.cloneLocation}/services/deleteipsum`, true);
+    done();
+  });
+
+
+  /**
+   * Check if copyFilesAsync operation has been performed (preparePushResult -> copier.copyFilesAsync)
+   */
+  it(`AdditionalIpsum service folder should be copied to ${config.generationResult.cloneLocation} folder`, (done) => {
+    _checkFileOrDir(false, `${config.tempLocation}/${config.generationResult.cloneLocation}/services/additionalipsum`, true);
+    done();
+  });
+
+  it('AdditionalIpsum service folder should have some content', (done) => {
+    _checkFileOrDir(true, `${config.tempLocation}/${config.generationResult.cloneLocation}/services/additionalipsum/v1/index.html.eco`, true);
+    done();
+  });
+
+
+  /**
+   * Check if restoreBackupOfNotClonedRepositories operation has been performed (preparePushResult -> deletePreviouslyClonedResultsRepo)
+   */
+  it('FailingIpsum service folder should be restored', (done) => {
+    _checkFileOrDir(false, `${config.tempLocation}/${config.generationResult.cloneLocation}/services/failingipsum`, true);
+    done();
+  });
+
+  it('FailingIpsum service folder should have some content', (done) => {
+    _checkFileOrDir(true, `${config.tempLocation}/${config.generationResult.cloneLocation}/services/failingipsum/v1/index.html.eco`, true);
+    done();
+  });
+
+
+  after((done) => {
+    rimraf(config.tempLocation, () => {
+      rimraf(config.skeletonOutDestination, done);
+    });
+  });
+
+});
+
 
 function _checkFileOrDir(isFile, pathToBeChecked, expectedResult){
   const fileOrDir = isFile ? testHelper.fileCheckSync(path.resolve(pathToBeChecked)) : testHelper.dirCheckSync(path.resolve(pathToBeChecked));
