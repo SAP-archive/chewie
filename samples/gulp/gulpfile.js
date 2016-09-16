6939ee2de9951686801ca669ed60536d6cc6fc65
Gornicki, Lukasz <lukasz.gornicki@hybris.com>
2016-09-16 12:39:04 +0200

    added readFile to registry preparation

--- samples/gulp/gulpfile.js
+++ samples/gulp/gulpfile.js
@@ -1,7 +1,9 @@
+/* eslint camelcase: 0 */
 'use strict';
 
 const gulp = require('gulp'),
   chewie = require('chewie'),
+  validation = require('./gulp_tasks/deployment/validationMetadata.js'),
   argv = require('yargs')
   .alias('s', 'section')
   .alias('t', 'topics')
@@ -11,13 +13,18 @@ const gulp = require('gulp'),
   .alias('p', 'platform')
   .alias('a', 'sauce')
   .alias('y', 'type')
+  .alias('f', 'force')
   .argv,
   log = require('./node_modules/chewie/src/helpers/logger'),
+  unzip = require('gulp-unzip'),
+  download = require('gulp-download'),
   async = require('async'),
-  path = require('path');
+  path = require('path'),
+  INTERACTIVE_DOCU_SRC_LOC = 'https://devportal.yaas.io/build.zip';
 
 
 const LOCAL_REGISTRY_PATH = '../sample_data';
+
 
 const localValue = argv.local;
 
@@ -36,39 +43,49 @@ const topics = _getTopics(argv.topics);
 
 gulp.task('start', (cb) => {
 
-  let registry;
 
-  //If you use remote registry, you want to pass the name of the branch in the remote repo, for example you can run task with: '-b dev'
-  chewie.prepareRegistry(topics, config, () => {
+  chewie.removeClonedRepositories(argv.force, config, () => {
 
-    const fullRegistry = require(config.registry.registryPath);
+    let registry;
 
-    topics ? registry = require(`${config.tempLocation}/${config.registry.shortVersionFileName}`) : registry = fullRegistry;
+    //If you use remote registry, you want to pass the name of the branch in the remote repo, for example you can run task with: '-b dev'
+    chewie.prepareRegistry(topics, config, () => {
 
-    //stop processing if there is no registry
-    if (!registry) {
-      throw new Error('Registry is not provided');
-    }
+      const fullRegistry = require(config.registry.registryPath);
 
-    async.series({
-      cloneDocuSources: asyncCb(chewie.cloneDocuSources, registry, config),
-      rewriteRAML: asyncCb(chewie.rewriteRAML, registry, config, argv.r),
-      preparePlaceholders: asyncCb(chewie.preparePlaceholders, registry, config),
-      createMetaInfo: asyncCb(chewie.createMetaInfo, fullRegistry, topics, config),
-      copyTutorials: asyncCb(chewie.copyTutorials, registry, config),
-      prepareApiReferences: asyncCb(chewie.prepareApiReferences, registry, config),
-      createUrlPartials: asyncCb(chewie.createUrlPartials, registry, config),
-      createRAMLPartials: asyncCb(chewie.createRAMLPartials, registry, config),
-      copyContent: asyncCb(chewie.copyContent, fullRegistry, config)
-    }, cb);
+      topics ? registry = require(`${config.tempLocation}/${config.registry.shortVersionFileName}`) : registry = fullRegistry;
+
+      //stop processing if there is no registry
+      if (!registry) {
+        throw new Error('Registry is not provided');
+      }
+
+      async.series({
+        cloneDocuSources: asyncCb(chewie.cloneDocuSources, registry, config, topics),
+        rewriteRAML: asyncCb(chewie.rewriteRAML, registry, config, argv.r),
+        copyTutorials: asyncCb(chewie.copyTutorials, registry, config),
+        preparePlaceholders: asyncCb(chewie.preparePlaceholders, registry, config),
+        createMetaInfo: asyncCb(chewie.createMetaInfo, fullRegistry, topics, config),
+        prepareApiReferences: asyncCb(chewie.prepareApiReferences, registry, config),
+        createUrlPartials: asyncCb(chewie.createUrlPartials, registry, config),
+        createRAMLPartials: asyncCb(chewie.createRAMLPartials, registry, config),
+        copyContent: asyncCb(chewie.copyContent, fullRegistry, config)
+      }, cb);
+    });
   });
+
 
   function asyncCb() {
     const args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments)); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
     const func = args[0];
     const params = args.slice(1);
+
     return (cb) => {
-      params.push(cb);
+      log.info(`Started task: ${func.name}`);
+      params.push(() => {
+        log.info(`Finished task: ${func.name}`);
+        cb();
+      });
       func.apply(null, params);
     };
   }
@@ -76,8 +93,11 @@ gulp.task('start', (cb) => {
 
 
 gulp.task('replaceApiReferences', (cb) => {
-  const registry = require(config.registry.registryPath);
-  chewie.replaceApiReferences(registry, config, cb);
+  chewie.prepareRegistry(topics, config, (err) => {
+
+    const registry = require(config.registry.registryPath);
+    chewie.replaceApiReferences(registry, config, cb);
+  });
 });
 
 
@@ -92,7 +112,7 @@ gulp.task('serviceLatest', ['fixTables'], (cb) => {
 });
 
 
-gulp.task('minify', (cb) => {
+gulp.task('minify', ['serviceLatest'], (cb) => {
   chewie.minify(config, cb);
 });
 
@@ -104,7 +124,9 @@ gulp.task('minify', (cb) => {
 //
 // mind that rn and partials are not sections anymore, they are deleted per service
 gulp.task('clean', (cb) => {
-  chewie.prepareRegistry(topics, config, () => {
+
+  chewie.prepareRegistry(topics, config, (err) => {
+
     const registry = require(config.registry.registryPath);
     chewie.cleanSkeleton.clean(registry, config, argv.s && argv.s.toLowerCase(), cb);
   });
@@ -112,7 +134,35 @@ gulp.task('clean', (cb) => {
 
 
 // task pushes latest results to remote repo that keeps results.
-gulp.task('pushResult', () => {
+gulp.task('pushResult', (cb) => {
+  const topics = _getTopics(argv.topics);
+  const opt = {
+    'src': `${config.skeletonOutDestination}/**`,
+    'dest': config.generationResult.clonedResultFolderPath,
+    'branch': config.generationResult.branch,
+    'message': Boolean(!argv.topics) ? 'Push operation for the whole Dev Portal' : `Push operation for: ${JSON.stringify(topics)}`,
+    'independent': Boolean(argv.topics)
+  };
+
+  chewie.pushResult(opt, (err) => {
+    if (err) {
+      log.error(err);
+      return cb();
+    }
+    log.info('Push operation completed');
+    cb();
+  });
+});
+
+gulp.task('getDependencyInteractiveDocu', (cb) => {
+
+  download(INTERACTIVE_DOCU_SRC_LOC)
+    .pipe(unzip())
+    .pipe(gulp.dest('./src/raw'))
+    .on('end', cb);
+});
+
+gulp.task('preparePushResult', (cb) => {
   const topics = _getTopics(argv.topics);
   const opt = {
     'src': `${config.skeletonOutDestination}/**`,
@@ -120,12 +170,18 @@ gulp.task('pushResult', () => {
     'branch': config.generationResult.branch,
     'message': Boolean(!argv.topics) ? 'Push operation for the whole Dev Portal' : `Push operation for: ${JSON.stringify(topics)}`,
     'independent': Boolean(argv.topics),
-    'notUsedFiles': config.independentGeneration.notUsedFiles
+    'tempLocation': config.tempLocation,
+    'notClonedRepositoriesFile': config.notClonedRepositoriesFile,
+    'indepenedentDocuRepositoriesFile': config.indepenedentDocuRepositoriesFile
   };
 
-  chewie.pushResult(opt, (err) => {
-    if (err) return log.error(err);
-    log.info('Push operation completed');
+  chewie.preparePushResult(opt, (err) => {
+    if (err) {
+      log.error(err);
+      return cb();
+    }
+    log.info('Prepare pushResult operation completed');
+    cb();
   });
 });
 
@@ -142,3 +198,47 @@ function _getTopics(topics) {
     };
   });
 }
+
+
+
+/////////////////////////////////////////
+///            TESTS                  ///
+/////////////////////////////////////////
+
+gulp.task('test', (cb) => {
+
+  const nightwatch = require('gulp-nightwatch');
+  const helper = require('./tests/devportal/helpers/helper');
+  const innerConfig = require('./tests/devportal/helpers/variables');
+  const jsonTransform = require('gulp-json-transform');
+  const os = require('os');
+
+  const enviroment = helper.determineEnviroment(argv.b, argv.p);
+
+  log.info(`Running tests against nightwatch envroment(s): ${enviroment} `, argv.a ? 'Test will be run on Sauce Labs (parameter s is present)' : 'Test will be run on localhost');
+
+  gulp.src('./nightwatch.json')
+    .pipe(jsonTransform((data) => {
+      data.test_settings.default.launch_url = innerConfig.launchUrl;
+
+      data.src_folders = argv.y ? [`tests/devportal/test-cases/${argv.y}`] : ['tests/devportal/test-cases'];
+
+      data.test_settings.default.selenium_port = argv.a ? 80 : 4450;
+      data.test_settings.default.selenium_host = argv.a ? 'ondemand.saucelabs.com' : 'localhost';
+      data.selenium.start_process = argv.a ? false : true;
+
+      data.test_settings.default.username = innerConfig.username;
+      data.test_settings.default.access_key = innerConfig.accessKey;
+
+      return data;
+    }), 4)
+    .pipe(gulp.dest('./'))
+    .pipe(nightwatch({
+      configFile: 'nightwatch.json',
+      cliArgs: {
+        env: enviroment,
+        retries: 3
+      }
+    }))
+    .on('error', cb);
+});
--- src/integration/prepareRegistry.js
+++ src/integration/prepareRegistry.js
@@ -5,6 +5,7 @@ const cloner = require('../helpers/cloner'),
   concater = require('../helpers/concater'),
   creator = require('../helpers/creator'),
   misc = require('../helpers/misc'),
+  reader = require('../helpers/reader'),
   path = require('path');
 
 /**
@@ -12,48 +13,61 @@ const cloner = require('../helpers/cloner'),
  * You need to make sure to have a proper configuration settings in chewieConfig().js ('registry' attribute).
  * There are 2 different cases allowed, 'local' and 'remote'.
  * For 'remote' case, you need to pass a branch or tag name for the remote repository.
+ * @param {Object} [topics] - array with list of objects with topic name and type.
+ * Used in case you do not want to prepare registry with all topics but only with specific ones
  * @param {Object} [config] - basic integration configuration
  * @param {Function} [next] - callback for asynch operations
  */
 
 function prepareRegistry(topics, config, next) {
 
-  const registrySource = config.registry.path;
-  const registryOrigin = config.registry.location;
-  const branchTag = config.registry.branch;
+  const registrySource = config.registry.path,
+    registryOrigin = config.registry.location,
+    registryPath = config.registry.registryPath,
+    branchTag = config.registry.branch;
 
-  switch(registryOrigin){
+  //First step of preparing the registry is to actually check if it isn't already prepared
+  //If registry exists, we pass to callback
+  reader.readFile(registryPath, (err) => {
 
-  case 'local':
+    if (!err) {
 
-    prepareRegistryForLocal(registrySource, config, () => {
-      topics ? shrinkedRegistry(topics, config, next) : next();
-    });
+      return next();
+    }
 
-    break;
+    switch(registryOrigin){
 
-  case 'remote':
+    case 'local':
 
-    prepareRegistryForExternal(registrySource, branchTag, config, () => {
-      topics ? shrinkedRegistry(topics, config, next) : next();
-    });
+      _prepareRegistryForLocal(registrySource, config, () => {
+        topics ? _shrinkedRegistry(topics, config, next) : next();
+      });
 
-    break;
+      break;
 
-  default:
+    case 'remote':
 
-    throw new Error(`You need to specify a proper location of your registry.
-           It should be local or remote and not something that you have currently: ${config.registry.location}`);
-  }
+      _prepareRegistryForExternal(registrySource, branchTag, config, () => {
+        topics ? _shrinkedRegistry(topics, config, next) : next();
+      });
+
+      break;
+
+    default:
+
+      throw new Error(`You need to specify a proper location of your registry.
+             It should be local or remote and not something that you have currently: ${config.registry.location}`);
+    }
+  });
 }
 
-function prepareRegistryForLocal(registrySource, config, next) {
+function _prepareRegistryForLocal(registrySource, config, next) {
 
   //calling concat function to prepare a final registry file
   concater.concatRegistry(registrySource, config, next);
 }
 
-function prepareRegistryForExternal(registrySource, branchTag, config, next) {
+function _prepareRegistryForExternal(registrySource, branchTag, config, next) {
   const clonePath = config.registry.clonedRegistryFolderPath;
   const registryCloneLocation = config.registry.pathFolderWithClonedRegistry;
 
@@ -65,7 +79,7 @@ function prepareRegistryForExternal(registrySource, branchTag, config, next) {
   });
 }
 
-function shrinkedRegistry(topics, config, next) {
+function _shrinkedRegistry(topics, config, next) {
 
   let registry = misc.getRegistry(path.resolve(process.cwd(), `${config.tempLocation}/${config.registry.fileName}`));
   registry = misc.registryShrink(registry, topics);
