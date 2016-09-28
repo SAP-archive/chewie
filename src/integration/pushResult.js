e159e0ab659e256f91d7a95adbb44d8afc5813b9
Przezorski, Lukasz <lukasz.przezorski@sap.com>
2016-09-28 16:04:11 +0200

    test

--- src/integration/pushResult.js
+++ src/integration/pushResult.js
@@ -23,6 +23,7 @@ function pushResult(opt, next) {
   async.series([
     addCommit(dest, message),
     pull(branch, dest),
+    merge(branch, dest),
     push(branch, dest)
   ], next);
 }
@@ -45,13 +46,22 @@ function addCommit(src, msg){
 //pulling from remote repo
 function pull(branch, src){
   return (cb) => {
-    git.pull('origin', branch, {cwd: src, maxBuffer: Infinity, args: '--rebase'}, (err) => {
+    git.pull('origin', branch, {cwd: src, maxBuffer: Infinity}, (err) => {
       if (err) log.warning(`Pull operation for branch ${branch} has failed with the error: ${err}`);
       cb(err);
     });
   };
 }
 
+//merging updates made online with your local work
+function merge(branch, src){
+  return (cb) => {
+    git.merge('origin', {cwd: src, maxBuffer: Infinity, args: '--depth=1'}, (err) => {
+      if (err) log.warning(`Merge operation for branch ${branch} has failed with the error: ${err}`);
+      cb(err);
+    });
+  };
+}
 
 //pushing to remote repo
 function push(branch, src){
