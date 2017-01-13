ce6a880cc61129cfef851d7fe8dc29008ba36700
Przezorski, Lukasz <lukasz.przezorski@sap.com>
2017-01-13 09:43:51 +0100

    Initial: add try catch

--- src/integration/rewriteRAML.js
+++ src/integration/rewriteRAML.js
@@ -139,10 +139,15 @@ function _parse (source, docuDir, raml, baseUri, traits) {
  * @param {string} [to] - new string to replace the old one
  */
 function _replaceInFile(file, from, to) {
-  const data = fs.readFileSync(file, 'utf-8');
+  try {
+    const data = fs.readFileSync(file, 'utf-8');
 
-  const regexExp = new RegExp(`${from}(?!:)`, 'g');
-  const replace = data.replace(regexExp, to);
+    const regexExp = new RegExp(`${from}(?!:)`, 'g');
+    const replace = data.replace(regexExp, to);
 
-  creator.createFilesSync(file, replace, 'utf-8');
+    creator.createFilesSync(file, replace, 'utf-8');
+  }
+  catch(err) {
+    logger.error(`The error occured: ${err}`);
+  }
 }
