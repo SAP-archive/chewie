a1ec1182ad14eee1ccb5e6a3fc118a1796296f2b
Luke <przezorski@gmail.com>
2017-02-27 11:41:32 +0100

    Update chewieConfigTest.js

--- test/chewieConfigTest.js
+++ test/chewieConfigTest.js
@@ -33,7 +33,7 @@ const config = {
 
   generationResult: {
     srcLocation: 'https://github.com/YaaS/chewie-sample-result.git',
-    branch: 'master',
+    branch: 'preparePushResultTestInit',
     cloneLocation: 'latestStarWarsRepo'
   },
 
