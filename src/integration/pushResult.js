0994bf38014a7b48a9ac87088066f4a45f2311c3
Przezorski, Lukasz <lukasz.przezorski@sap.com>
2017-03-28 10:10:18 +0200

    Add --quiet to git push

--- src/integration/pushResult.js
+++ src/integration/pushResult.js
@@ -55,7 +55,7 @@ function pull(branch, src){
 //pushing to remote repo
 function push(branch, src){
   return (cb) => {
-    git.push('origin', branch, {cwd: src, maxBuffer: Infinity}, (err) => {
+    git.push('origin', branch, {cwd: src, quiet: true, maxBuffer: Infinity}, (err) => {
       if (err) log.warning(`Push operation for branch ${branch} has failed with the error: ${err}`);
       cb(err);
     });
